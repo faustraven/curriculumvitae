190969b7fbdd52ecafe17685a530284a0ec0bc94
Fausto Becerro <131420606+faustraven@users.noreply.github.com>
2023-08-23 17:04:12 -0300

    Proyecto de html y css de curriculum vitae
    proyecto para curso de full stack java

---
+++ CURRICULUM VITAE faustraven/programacion.js
@@ -0,0 +1,12 @@
+function mostrarFormacionAcademica(){
+    document.getElementById("padreFormacion").style.display="block";
+}
+function mostrarConocimientosAdicionales(){
+    document.getElementById("padreConocimientos").style.display="block";
+}
+function mostrarExperienciaProfesional(){
+    document.getElementById("padreExperiencia").style.display="block";
+}
+function mostrarReferencias(){
+    document.getElementById("padreReferencias").style.display="block";
+}
